502d7f57b170252343f26067935971b329553de9
mrkcntrmn <ilovepoker@gmail.com>
2018-10-08 15:27:17 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -9,11 +9,11 @@ Created on Mar 4, 2017
 
 #This is how you print output using Python version 2.X
 
-print "Hello world!"
+print "Python2 Hello world!"
 
 
 #This is how you print output using Python version 3.X
 
-print("Hello world!")
+print("Python3 Hello world!")
 
 print("both versions work in 2.x so use 2.x... duh!")
