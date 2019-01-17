2f715e62397f550c4c27dc15b741b6efb1a6a224
mrkcntrmn <ilovepoker@gmail.com>
2019-01-16 20:38:13 -0600

    Create log.py

---
+++ log.py
@@ -0,0 +1,13 @@
+'''
+Created on Jan 16, 2019
+@author: MARK
+'''
+import logging
+
+#This is a comment using Python!
+
+
+#This is how you print output using Python version 2.X
+
+print "Logging started"
+
