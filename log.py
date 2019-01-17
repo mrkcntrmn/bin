2fc8a272e10af987f101027b8c07d9fc897fc4c1
mrkcntrmn <ilovepoker@gmail.com>
2019-01-16 21:03:57 -0600

    Update log.py

--- log.py
+++ log.py
@@ -1,10 +1,10 @@
-#Created on Jan 16, 2019
-#@author: MARK
-
-#import logging
+...
+Created on Jan 16, 2019
+@author: MARK
+...
+import logging
 
 #This is a comment using Python!
-
 
 #This is how you print output using Python version 2.X
 
