84fdcce4f2cd8cc22bc62335451b5f720f0e3713
mrkcntrmn <ilovepoker@gmail.com>
2019-01-20 13:24:32 -0600

    Update boto3.py

--- boto3.py
+++ boto3.py
@@ -1,23 +1,13 @@
-'''
-Created on Jan 19, 2019
-@author: MARK
-'''
-
-#This is a comment using Python!
-
-
-print "Python2 running..."
-
-
-print "Boto3 importing..."
-
 import boto3
 
+# Create an S3 client
+s3 = boto3.client('s3')
 
+# Call S3 to list current buckets
+response = s3.list_buckets()
 
-# Let's use Amazon S3
-s3 = boto3.resource('s3')
+# Get a list of all bucket names from the response
+buckets = [bucket['Name'] for bucket in response['Buckets']]
 
-# Print out bucket names
-for bucket in s3.buckets.all():
-    print(bucket.name)
+# Print out the bucket list
+print("Bucket List: %s" % buckets)
