d97a9fe5382072088c557aec5467de973b2926ff
mrkcntrmn <ilovepoker@gmail.com>
2019-01-20 17:17:16 -0600

    Update rek.py

--- rek.py
+++ rek.py
@@ -11,12 +11,12 @@ response=rek.detect_text(Image={'S3Object':{'Bucket':bucket,'Name':photo}})
                         
 textDetections=response['TextDetections']
 print ('Detected text')
-    for text in textDetections:
-            print ('Detected text:' + text['DetectedText'])
-            print ('Confidence: ' + "{:.2f}".format(text['Confidence']) + "%")
-            print ('Id: {}'.format(text['Id']))
-            if 'ParentId' in text:
-                print ('Parent Id: {}'.format(text['ParentId']))
-            print ('Type:' + text['Type'])
-            print
+for text in textDetections:
+    print ('Detected text:' + text['DetectedText'])
+    print ('Confidence: ' + "{:.2f}".format(text['Confidence']) + "%")
+    print ('Id: {}'.format(text['Id']))
+         if 'ParentId' in text:
+             print ('Parent Id: {}'.format(text['ParentId']))
+         print ('Type:' + text['Type'])
+         print    
 
