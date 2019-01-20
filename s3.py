5b91311c0133f8588cea4809528aa0b983da188a
mrkcntrmn <ilovepoker@gmail.com>
2019-01-20 14:02:19 -0600

    Update s3.py

--- s3.py
+++ s3.py
@@ -6,3 +6,8 @@ s3 = boto3.resource('s3')
 # Print out bucket names
 for bucket in s3.buckets.all():
     print(bucket.name)
+   
+# Upload a new file
+data = open('test.jpg', 'rb')
+s3.Bucket('26lenox').put_object(Key='test.jpg', Body=data)
+
