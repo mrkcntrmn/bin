98abb6aeb86b65071a1d5f939dcb620b2bbd58d8
mrkcntrmn <ilovepoker@gmail.com>
2019-01-20 18:03:18 -0600

    Update rek.py

--- rek.py
+++ rek.py
@@ -1,22 +1,16 @@
 import boto3
 
-if __name__ == "__main__":
+bucket='26lenox'
+photo='test.jpg'
 
-    bucket='26lenox'
-    photo='test.jpg'
+client=boto3.client('rekognition')
 
-    client=boto3.client('rekognition')
-
-  
-    response=client.detect_text(Image={'S3Object':{'Bucket':bucket,'Name':photo}})
-                        
-    textDetections=response['TextDetections']
-    print ('Detected text')
-    for text in textDetections:
-            print ('Detected text:' + text['DetectedText'])
-            print ('Confidence: ' + "{:.2f}".format(text['Confidence']) + "%")
-            print ('Id: {}'.format(text['Id']))
-            if 'ParentId' in text:
-                print ('Parent Id: {}'.format(text['ParentId']))
-            print ('Type:' + text['Type'])
-            print
+response = client.detect_text(
+    Image={
+        'Bytes': b'bytes',
+        'S3Object': {
+            'Bucket': bucket,
+            'Name': photo            
+        }
+    }
+)  
