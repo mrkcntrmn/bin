4862bee224d680d8c3fa2bea7d48e1d7da3a9828
mrkcntrmn <ilovepoker@gmail.com>
2019-01-20 18:04:26 -0600

    Update rek.py

--- rek.py
+++ rek.py
@@ -1,7 +1,5 @@
 import boto3
 
-bucket='26lenox'
-photo='test.jpg'
 
 client=boto3.client('rekognition')
 
@@ -9,8 +7,8 @@ response = client.detect_text(
     Image={
         'Bytes': b'bytes',
         'S3Object': {
-            'Bucket': bucket,
-            'Name': photo            
+            'Bucket': 26lenox,
+            'Name': test.jpg            
         }
     }
 )  
