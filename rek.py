5e3fe1f2775d372fbf1534c3c98041595def0f7a
mrkcntrmn <ilovepoker@gmail.com>
2019-01-20 18:54:25 -0600

    Update rek.py

--- rek.py
+++ rek.py
@@ -1,6 +1,12 @@
 import boto3
 
-
 client=boto3.client('rekognition')
 
-response=client.detect_text(Image='https://s3.us-east-2.amazonaws.com/26lenox/cert.jpg')
+response = client.detect_text(
+    Image={        
+        'S3Object': {
+            'Bucket': '15rek',
+            'Name': 'Scan.png'            
+        }
+    }
+)  
